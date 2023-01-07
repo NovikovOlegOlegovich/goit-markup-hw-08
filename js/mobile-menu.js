0fabcb438d385174aa40c2f544d0ecb64996ff4f
Novikov Oleh <novikov19962906@gmail.com>
2023-01-07 02:13:50 +0200

    1.3

---
+++ js/mobile-menu.js
@@ -0,0 +1,13 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.menu-open'),
+    closeMenuBtn: document.querySelector('.menu-close'),
+    menu: document.querySelector('.data-modal'),
+  };
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('backdrop--is-hidden');
+  }
+})();
